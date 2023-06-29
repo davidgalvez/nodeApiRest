1fe57a3941bf406a2a45e89281be92b666676f09
davidgalvez <davidgalvezfis@gmail.com>
2023-06-29 03:24:25 +0200

    adding api route validators

--- routes/tracks.js
+++ routes/tracks.js
@@ -1,10 +1,11 @@
 const express =require("express");
 const { getItem, getItems, createItem } = require("../controllers/tracks");
+const { validatorCreateItem } = require("../validators/tracks");
 const router= express.Router();
 
 
 router.get("/",getItems)
 router.get("/:id",getItem)
-router.post("/",createItem)
+router.post("/",validatorCreateItem,createItem)
 
 module.exports=router;
