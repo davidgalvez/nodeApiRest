0313ac923723d5dba2194b08c54ed19cc160787f
davidgalvez <davidgalvezfis@gmail.com>
2023-06-30 02:56:05 +0200

    creating a middleware for create tracks route

--- routes/tracks.js
+++ routes/tracks.js
@@ -1,11 +1,12 @@
 const express =require("express");
 const { getItem, getItems, createItem } = require("../controllers/tracks");
 const { validatorCreateItem } = require("../validators/tracks");
+const customHeader = require("../middleware/customHeader");
 const router= express.Router();
 
 
 router.get("/",getItems)
 router.get("/:id",getItem)
-router.post("/",validatorCreateItem,createItem)
+router.post("/",validatorCreateItem,customHeader,createItem)
 
 module.exports=router;
