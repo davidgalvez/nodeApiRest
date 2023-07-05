60ce6e0c403131d3686dcdc887f72f4a06b89306
davidgalvez <davidgalvezfis@gmail.com>
2023-07-05 18:40:46 +0200

    implementing tracks CRUD api controllers and routes

--- routes/tracks.js
+++ routes/tracks.js
@@ -1,13 +1,32 @@
 const express =require("express");
-const { getItem, getItems, createItem } = require("../controllers/tracks");
-const { validatorCreateItem } = require("../validators/tracks");
+const { getItem, getItems, createItem, updateItem, deleteItem } = require("../controllers/tracks");
+const { validatorCreateItem, validatorGetItem } = require("../validators/tracks");
 const customHeader = require("../middleware/customHeader");
 const router= express.Router();
 
-
+/**
+ * List items
+ */
 router.get("/",getItems)
-router.get("/:id",getItem)
+
+/**
+ * Get item details
+ */
+router.get("/:id",validatorGetItem,getItem)
+
+/**
+ * Create item
+ */
 router.post("/",validatorCreateItem,customHeader,createItem)
 
+/**
+ * Update item
+ */
+router.put("/:id",validatorGetItem,validatorCreateItem,customHeader,updateItem)
+
+/**
+ * Delete item
+ */
+router.delete("/:id",validatorGetItem,deleteItem)
 
 module.exports=router;
--- validators/tracks.js
+++ validators/tracks.js
@@ -41,4 +41,15 @@ const validatorCreateItem = [
     }
 ]
 
-module.exports = {validatorCreateItem}
+const validatorGetItem = [
+    
+    check("id")
+    .exists()
+    .notEmpty()
+    .isMongoId(),
+    (req,res,next) =>{
+        return validateResults(req,res,next)
+    }
+]
+
+module.exports = {validatorCreateItem,validatorGetItem}
